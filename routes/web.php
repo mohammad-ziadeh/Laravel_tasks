7ad3b3d0b37d3650ee777c2d6823d69d92135442
mohammad ziadeh <mohammedziadeh6@gmail.com>
2025-03-05 11:07:09 +0300

    task1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('hello');
-// });
+
 
 Route::get('/', function () {
     return view('home');
@@ -23,4 +22,87 @@ Route::get('/', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+
+//1---------------------------------------------------------------
+
+// Route::get('/task1', function () {
+//     return 'Hello world!';
+// });
+
+//2---------------------------------------------------------------
+
+// Route::get('/task2/{Name}', function ($Name) {
+//     return 'hello,' . $Name;
+// });
+
+//3---------------------------------------------------------------
+
+// Route::get('/task3/{age?}', function ($age) {
+//     if (isset($age)) {
+//         return "You are $age years old";
+//     } else {
+//         return "Age not provided";
+//     };
+// });
+
+//4---------------------------------------------------------------
+
+Route::get('/task4', [PostController::class, 'task']);
+
+//5---------------------------------------------------------------
+
+Route::get('/task5', function () {
+    return 'Hello world!';
+})->name('task5');
+
+//6---------------------------------------------------------------
+
+Route::group([
+    'prefix' => 'task6/',
+], function () {
+    Route::get('', function () {
+        return 'First child route';
+    });
+    Route::get('V2Task6', function () {
+        return 'Sec child route';
+    });
+});
+
+//7---------------------------------------------------------------
+
+Route::get('/task7/{id}', function ($id) {
+    return 'User ID id' . $id;
+});
+
+//8---------------------------------------------------------------
+
+Route::fallback( function () {
+    return 'Page not found';
+});
+//9---------------------------------------------------------------
+
+Route::resource( '/task9', PostController::class);
+
+
+//10---------------------------------------------------------------
+
+Route::get('/task10/{Cat}/{id}', function ($Cat,$id) {
+    return "this is $Cat with id $id";
+});
+
+//11---------------------------------------------------------------
+
+Route::get('/task11/{id?}', function ($id = 1) {
+    return 'hi student and your id is ->' . $id;
+});
+
+//12---------------------------------------------------------------
+
+Route::redirect('/task12', '/task11');
+
+//13---------------------------------------------------------------
+
+// Route::get('/', function () {
+//     return view('contact');
+// });
 
